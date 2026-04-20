3c5c9364bcadbad65c07cd29c581b0d84cb7f8c6
gaspcosto <gaspard@costockage.fr>
2026-04-20 09:45:48 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 export default async function handler(req, res) {
-  const conv_id = req.body?.conversation_id;
-  const workspace_id = req.body?.workspace_id;
 
   // Forward à Make 
   await fetch("https://hook.eu1.make.com/asxq5xg31x1q4bqzunwetcmj78i1ouui", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ conv_id, workspace_id })
+    body: JSON.stringify(req.body)
   });
 
   // Retourne le canvas à Intercom
